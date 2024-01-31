03109137fae6a6578e9469ea26a59f3841b48026
ddkogit <git config --global user.email>
2024-01-31 16:05:12 +0545

    Initialized

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import QuoteBox from "./components/QuoteBox"
+
+function App() {
+
+  return (
+    <>
+    <h2>Project 3 : Quote Generator</h2>
+    
+    <QuoteBox />
+    </>
+  )
+}
+
+export default App
